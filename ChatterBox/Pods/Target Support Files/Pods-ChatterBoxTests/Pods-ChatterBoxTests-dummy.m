8d2275ea4ac76ecf690f8bae9bb853aac943f9a7
Rajeev Ram <rajeevram@email.arizona.edu>
2018-08-22 15:07:11 -0700

    deleting everything to start from scratch

--- ChatterBox/Pods/Target Support Files/Pods-ChatterBoxTests/Pods-ChatterBoxTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ChatterBoxTests : NSObject
-@end
-@implementation PodsDummy_Pods_ChatterBoxTests
-@end
--- ChatterBox/Pods/Target Support Files/Pods-ChatterBoxUITests/Pods-ChatterBoxUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ChatterBoxUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ChatterBoxUITests
-@end
